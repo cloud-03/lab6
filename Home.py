bba3a3cd7121b6e595cadde185db2a617644746d
Ritz <claudio.bottino03@gmail.com>
2024-05-27 10:39:03 +0200

    schea base delle pagine per il laboratorio

--- Home.py
+++ Home.py
@@ -3,7 +3,7 @@ import numpy as np
 import pandas as pd
 
 st.set_page_config(
-    page_title="La mia App",
+    page_title="Prenota stanze",
     layout="wide",
     initial_sidebar_state="expanded",
     menu_items={
@@ -12,4 +12,13 @@ st.set_page_config(
         'About': "# Corso di *Basi di Dati*"
     }
 )
-st.title("📈 La mia App")
+st.markdown("# Esercitazione lab6")
+
+col1,col2=st.columns([3,2])
+with col1:
+    st.title(":red[Live Coding] Session")
+    st.markdown("## Corso di :blue[Basi di Dati]")
+    st.markdown("### A.A. 2021/2022")
+    
+with col2:
+    st.image("images/polito.png")
